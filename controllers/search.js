768aa55fed165a7d313e6ae88ec65847f64d30fa
kazomikun <106589367+kazomikun@users.noreply.github.com>
2022-07-06 09:13:34 +0100

    Update search.js

--- controllers/search.js
+++ controllers/search.js
@@ -2,13 +2,12 @@
 const express = require('express')
 const router = express.Router()
 
-// const app = express()
-
 // Views
 // Create here a controller that accepts GET requests and renders the "search" page
 router.get('/', (req, res) => {
   let user = 'John Doe'
   res.render('search', { user })
+  //console.log(req)
 })
 
 // Export
