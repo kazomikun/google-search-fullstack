bb751e7032d859239b483465a72ff3da6f8e18e8
kazomikun <106589367+kazomikun@users.noreply.github.com>
2022-07-11 05:45:28 +0100

    Update results.js

--- controllers/results.js
+++ controllers/results.js
@@ -1,80 +1,29 @@
 // Import Packages
 const express = require('express')
 const router = express.Router()
+const Results = require('../models/results')
 
 // Create POST controller
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // console.log(res)
-  console.log(req.body)
-  let searchResults = [
-    {
-      title: 'Javascript Tutorial',
-      description:
-        'Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python, Bootstrap, Java',
-      url: 'https://www.w3schools.com',
-      links: [
-        {
-          title: 'JavaScript Introduction',
-          url: 'https://www.w3schools.com/js'
-        },
-        {
-          title: 'JS Functions',
-          url: 'https://www.w3schools.com/js'
-        },
-        {
-          title: 'JavaScript Examples',
-          url: 'https://www.w3schools.com/js'
-        }
-      ]
-    },
-    {
-      title: 'Javascript | MDN',
-      description:
-        'JavaScript (JS) is a lightweight, interpreted or just-in-time compilied programming language with first class functions. While it is well-known as the scripting language for web page, CouchDB and Adobe Acrobat',
-      url: 'developer.mozilla.org',
-      links: [
-        {
-          title: 'JavaScript Operator',
-          url: 'https://www.w3schools.com/js'
-        },
-        {
-          title: 'JavaScript Code',
-          url: 'https://www.w3schools.com/js'
-        },
-        {
-          title: 'JavaScript Meaning',
-          url: 'https://www.w3schools.com/js'
-        }
-      ]
-    },
-    {
-      title: 'Javascript Wikipedia',
-      description:
-        'JavaScript (often abbrievated as JS is a programming language that conforms to the ECMAScript specification, JavaScript is a high-level often just-in-time.',
-      url: 'en.wikipedia.org',
-      links: [
-        {
-          title: 'JavaScript Online',
-          url: 'https://www.w3schools.com/js'
-        },
-        {
-          title: 'JavaScript Definition',
-          url: 'https://www.w3schools.com/js'
-        }
-      ]
+  //console.log(req.body)
+
+  let searchResults = await Results.find({
+    title: {
+      $regex: req.body.searchBox
+      // $options: “1”,
     }
-  ]
-  let resultArray = searchResults.filter(e =>
-    e.description.includes(req.body.searchBox)
-  )
-  res.render('results', { searchResults: resultArray })
+  })
+  console.log(searchResults)
+
+  res.render('results', { searchResults })
 })
 
 // Views
 // Create here a controller that accepts GET requests and renders the "search" page
 router.get('/', (req, res) => {
   console.log(req.body)
-  let searchResults = []
+  //let searchResults = []
   res.render('results', { searchResults })
 })
 
